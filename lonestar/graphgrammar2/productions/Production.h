96017746284fc68108382135a5aa4af0357a595b
Krzysztof Podsiadło <podsiadl@agh.edu.pl>
2020-01-11 08:03:33 +0100

    Refactor of P1 - extraction to superclass

--- lonestar/graphgrammar2/productions/Production.h
+++ lonestar/graphgrammar2/productions/Production.h
@@ -17,6 +17,15 @@ protected:
         return longestEdges;
     }
 
+    int getBrokenEdge(const std::vector<galois::optional<EdgeIterator>> &edgesIterators) const {
+        const vector<int> &brokenEdges = getBrokenEdges(edgesIterators);
+        if (!brokenEdges.empty()) {
+            return brokenEdges[0];
+        } else {
+            return -1;
+        }
+    }
+
     std::vector<int> getBrokenEdges(const std::vector<galois::optional<EdgeIterator>> &edgesIterators) const {
         std::vector<int> result;
         for (int i = 0; i < 3; ++i) {
@@ -39,6 +48,108 @@ protected:
         return std::pair<int, int>{edge, (edge + 1) % 3};
     }
 
+
+    void breakElementWithHangingNode(int edgeToBreak, ProductionState &pState, galois::UserContext<GNode> &ctx) const {
+        const std::pair<int, int> &brokenEdgeVertices = getEdgeVertices(edgeToBreak);
+        GNode &hangingNode = connManager.findNodeBetween(pState.getVertices()[brokenEdgeVertices.first],
+                                                         pState.getVertices()[brokenEdgeVertices.second]).get();
+
+        breakElementUsingNode(edgeToBreak, hangingNode, pState, ctx);
+
+        hangingNode->getData().setHanging(false);
+    }
+
+    void breakElementWithoutHangingNode(int edgeToBreak, ProductionState &pState, galois::UserContext<GNode> &ctx) const {
+        GNode newNode = createNodeOnEdge(edgeToBreak, pState, ctx);
+
+        breakElementUsingNode(edgeToBreak, newNode, pState, ctx);
+    }
+
+    GNode createNodeOnEdge(int edgeToBreak, ProductionState &pState, galois::UserContext<GNode> &ctx) const {
+        Graph &graph = connManager.getGraph();
+        const vector<galois::optional<EdgeData>> &edgesData = pState.getEdgesData();
+        bool breakingOnBorder = edgesData[edgeToBreak].get().isBorder();
+        int neutralVertex = getNeutralVertex(edgeToBreak);
+
+        auto edgePair = connManager.findSrc(edgesData[edgeToBreak].get());
+        graph.removeEdge(edgePair.first, edgePair.second);
+        NodeData newNodeData = NodeData{false, edgesData[edgeToBreak].get().getMiddlePoint(), !breakingOnBorder};
+        GNode newNode = graph.createNode(newNodeData);
+        graph.addNode(newNode);
+        ctx.push(newNode);
+        for (int i = 0; i < 3; ++i) {
+            auto vertexData = pState.getVerticesData()[i];
+            auto edge = graph.addEdge(newNode, pState.getVertices()[i]);
+            graph.getEdgeData(edge).setBorder(i != neutralVertex ? breakingOnBorder : false);
+            graph.getEdgeData(edge).setMiddlePoint(
+                    (newNodeData.getCoords().getX() + vertexData.getCoords().getX()) / 2.,
+                    (newNodeData.getCoords().getY() + vertexData.getCoords().getY()) / 2.,
+                    (newNodeData.getCoords().getZ() + vertexData.getCoords().getZ()) / 2.);
+            graph.getEdgeData(edge).setLength(newNodeData.getCoords().dist(vertexData.getCoords()));
+        }
+        return newNode;
+    }
+
+    void breakElementUsingNode(int edgeToBreak, GNode const &hangingNode, const ProductionState &pState,
+                               galois::UserContext<GNode> &ctx) const {
+        const std::pair<int, int> &brokenEdgeVertices = getEdgeVertices(edgeToBreak);
+        Graph &graph = connManager.getGraph();
+        int neutralVertex = getNeutralVertex(edgeToBreak);
+        NodeData hNodeData = hangingNode->getData();
+        addEdge(graph, hangingNode, pState.getVertices()[neutralVertex], false,
+                hNodeData.getCoords().dist(pState.getVerticesData()[neutralVertex].getCoords()),
+                (hNodeData.getCoords() + pState.getVerticesData()[neutralVertex].getCoords()) / 2);
+
+        connManager.createInterior(hangingNode, pState.getVertices()[neutralVertex],
+                                   pState.getVertices()[brokenEdgeVertices.first], ctx);
+        connManager.createInterior(hangingNode, pState.getVertices()[neutralVertex],
+                                   pState.getVertices()[brokenEdgeVertices.second], ctx);
+
+        graph.removeNode(pState.getInterior());
+    }
+
+    void addEdge(Graph &graph, GNode const &node1, GNode const &node2, bool border, double length,
+                 const Coordinates &middlePoint) const {
+        const EdgeIterator &newEdge = graph.addEdge(node1, node2);
+        graph.getEdgeData(newEdge).setBorder(border);
+        graph.getEdgeData(newEdge).setLength(length);
+        graph.getEdgeData(newEdge).setMiddlePoint(
+                middlePoint);
+    }
+
+    int getNeutralVertex(int edgeToBreak) const {
+        return (edgeToBreak + 2) % 3;
+    }
+
+
+    bool checkIfBrokenEdgeIsTheLongest(int brokenEdge, const std::vector<optional<EdgeIterator>> &edgesIterators,
+                                       const std::vector<GNode> &vertices, const std::vector<NodeData> &verticesData) {
+        std::vector<double> lengths(4);
+        Graph &graph = connManager.getGraph();
+        for (int i = 0, j = 0; i < 3; ++i) {
+            if (i != brokenEdge) {
+                lengths[j++] = graph.getEdgeData(edgesIterators[i].get()).getLength();
+            } else {
+                const std::pair<int, int> &brokenEdgeVertices = getEdgeVertices(brokenEdge);
+                GNode &hangingNode = connManager.findNodeBetween(vertices[brokenEdgeVertices.first],
+                                                                 vertices[brokenEdgeVertices.second]).get();
+                lengths[2] = graph.getEdgeData(
+                        graph.findEdge(vertices[brokenEdgeVertices.first], hangingNode)).getLength();
+                lengths[3] = graph.getEdgeData(
+                        graph.findEdge(vertices[brokenEdgeVertices.second], hangingNode)).getLength();
+            }
+        }
+        return !less(lengths[2] + lengths[3], lengths[0]) && !less(lengths[2] + lengths[3], lengths[1]);
+    }
+
+
+    static void logg(const NodeData &interiorData, const std::vector<NodeData> &verticesData) {
+        std::cout << "interior: (" << interiorData.getCoords().toString() << "), neighbours: (";
+        for (auto vertex : verticesData) {
+            std::cout << vertex.getCoords().toString() + ", ";
+        }
+        std::cout << ") ";
+    }
 
 
 public:
--- lonestar/graphgrammar2/productions/Production1.h
+++ lonestar/graphgrammar2/productions/Production1.h
@@ -29,113 +29,6 @@ private:
         return -1;
     }
 
-//    std::vector<int> getLongestEdges(const std::vector<double> &lengths) const {
-//        std::vector<int> longestEdges;
-//        for (int i = 0; i < 3; ++i) {
-//            if (!less(lengths[i], lengths[(i + 1) % 3]) && !less(lengths[i], lengths[(i + 2) % 3])) {
-//                longestEdges.push_back(i);
-//            }
-//        }
-//        return longestEdges;
-//    }
-
-    GNode createInterior(GNode *node1, GNode *node2, GNode *node3, galois::UserContext<GNode> &ctx) const {
-        NodeData firstInteriorData = NodeData{true, false};
-        auto firstInterior = connManager.createNode(firstInteriorData, ctx);
-        connManager.getGraph().addEdge(firstInterior, *node1);
-        connManager.getGraph().addEdge(firstInterior, *node2);
-        connManager.getGraph().addEdge(firstInterior, *node3);
-        return firstInterior;
-    }
-
-    void breakElement(int edgeToBreak, GNode &interior, const std::vector<optional<EdgeData>> &edgesData,
-                      const std::vector<GNode> &vertices,
-                      const std::vector<NodeData> &verticesData, galois::UserContext<GNode> &ctx) const {
-        Graph &graph = connManager.getGraph();
-        bool breakingOnBorder = edgesData[edgeToBreak].get().isBorder();
-        int neutralVertex = getNeutralVertex(edgeToBreak);
-
-        auto edgePair = connManager.findSrc(edgesData[edgeToBreak].get());
-        graph.removeEdge(edgePair.first, edgePair.second);
-        NodeData newNodeData = NodeData{false, edgesData[edgeToBreak].get().getMiddlePoint(), !breakingOnBorder};
-        GNode newNode = graph.createNode(newNodeData);
-        graph.addNode(newNode);
-        ctx.push(newNode);
-        for (int i = 0; i < 3; ++i) {
-            auto vertexData = verticesData[i];
-            auto edge = graph.addEdge(newNode, vertices[i]);
-            graph.getEdgeData(edge).setBorder(i != neutralVertex ? breakingOnBorder : false);
-            graph.getEdgeData(edge).setMiddlePoint(
-                    (newNodeData.getCoords().getX() + vertexData.getCoords().getX()) / 2.,
-                    (newNodeData.getCoords().getY() + vertexData.getCoords().getY()) / 2.,
-                    (newNodeData.getCoords().getZ() + vertexData.getCoords().getZ()) / 2.);
-            graph.getEdgeData(edge).setLength(newNodeData.getCoords().dist(vertexData.getCoords()));
-        }
-//        NodeData secondInteriorData = NodeData{true, false};
-//        auto secondInterior = connManager.createNode(secondInteriorData, ctx);
-//        GNode firstInterior;
-        NodeData firstInteriorData = NodeData{true, false};
-        auto firstInterior = connManager.createNode(firstInteriorData, ctx);
-//        std::vector<GNode> vertices2;
-//        for (Graph::edge_iterator ii = graph.edge_begin(interior), ee = graph.edge_end(interior); ii != ee; ++ii) {
-//            Graph::edge_iterator ii = graph.edge_begin(interior);
-//            GNode v1 = graph.getEdgeDst(ii++);
-//            GNode v2 = graph.getEdgeDst(ii++);
-//            GNode v3 = graph.getEdgeDst(ii++);
-//        }
-        connManager.getGraph().addEdge(firstInterior, newNode);
-        connManager.getGraph().addEdge(firstInterior, vertices[neutralVertex]);
-//        connManager.getGraph().addEdge(firstInterior, vertices[0]);
-//        connManager.getGraph().addEdge(firstInterior, vertices2[(edgeToBreak + 1) % 3]);
-
-        NodeData secondInteriorData = NodeData{true, false};
-        auto secondInterior = connManager.createNode(secondInteriorData, ctx);
-
-        connManager.getGraph().addEdge(secondInterior, newNode);
-        connManager.getGraph().addEdge(secondInterior, vertices[neutralVertex]);
-//        connManager.getGraph().addEdge(secondInterior, connManager.getNeighbourN(interior, neutralVertex));
-//        connManager.getGraph().addEdge(secondInterior, vertices2[(edgeToBreak + 2) % 3]);
-
-//        GNode interior1 = createInterior(&newNode, vertices[neutralVertex], vertices[(edgeToBreak + 1) % 3], ctx);
-//        graph.addEdge(secondInterior, newNode);
-//        graph.addEdge(secondInterior, vertices[neutralVertex]);
-        bool switc = false;
-        for (int j = 0; j < 3; ++j) {
-            if (j != neutralVertex) {
-                graph.addEdge(!switc ? firstInterior : secondInterior, vertices[j]);
-                const Coordinates &coords = (newNodeData.getCoords() + verticesData[neutralVertex].getCoords() +
-                                             verticesData[j].getCoords()) / 3.;
-                if (!switc) {
-                    firstInterior->getData().setCoords(coords);
-                } else {
-                    secondInterior->getData().setCoords(coords);
-                }
-                switc = true;
-            }
-        }
-
-        graph.removeNode(interior);
-
-
-//        NodeData testD = NodeData{true, false};
-//        connManager.createNode(testD, ctx);
-    }
-
-    std::pair<int, int> getEdgeVertices(int edge) const {
-        return std::pair<int, int>{edge, (edge + 1) % 3};
-    }
-
-    int getNeutralVertex(int edgeToBreak) const {
-        return (edgeToBreak + 2) % 3;
-    }
-
-    static void logg(const NodeData &interiorData, const std::vector<NodeData> &verticesData) {
-        std::cout << "interior: (" << interiorData.getCoords().toString() << "), neighbours: (";
-        for (auto vertex : verticesData) {
-            std::cout << vertex.getCoords().toString() + ", ";
-        }
-        std::cout << ") ";
-    }
 
 public:
     using Production::Production;
@@ -154,7 +47,7 @@ public:
             return false;
         }
 
-        breakElement(edgeToBreak, pState.getInterior(), pState.getEdgesData(), pState.getVertices(), pState.getVerticesData(), ctx);
+        breakElementWithoutHangingNode(edgeToBreak, pState, ctx);
         std::cout << "P1 executed ";
 
         return true;
