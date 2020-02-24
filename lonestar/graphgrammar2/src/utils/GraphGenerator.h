83f9521be699dfc7065f6e9a4743ca3bec3a27b4
Loc Hoang <l_hoang@utexas.edu>
2020-02-24 17:11:28 -0600

    Commented GraphGenerator + renamed some vars for clarity

--- lonestar/graphgrammar2/src/utils/GraphGenerator.h
+++ lonestar/graphgrammar2/src/utils/GraphGenerator.h
@@ -32,21 +32,33 @@ public:
       Graph& graph, Map& map, const double west_border,
       const double north_border, const double east_border,
       const double south_border, bool version2D) {
+    // temp storage for nodes we care about for this function
     vector<GNode> nodes;
+    // wrapper around graph to edit it
     ConnectivityManager connManager{graph};
 
     Utils::convertToUtm(south_border, west_border,
                         map); // TODO: What is a point of this call?!
 
+    // note the following coordinates should be the same ones used to load
+    // terrain data into the map; the height (z coordinate) is retrieved from
+    // said map
+
+    // SW
     const Coordinates& coordinates0 =
         Coordinates{Utils::convertToUtm(south_border, west_border, map), map};
+    // NW
     const Coordinates& coordinates1 =
         Coordinates{Utils::convertToUtm(north_border, west_border, map), map};
+    // SE
     const Coordinates& coordinates2 =
         Coordinates{Utils::convertToUtm(south_border, east_border, map), map};
+    // NE
     const Coordinates& coordinates3 =
         Coordinates{Utils::convertToUtm(north_border, east_border, map), map};
 
+    // create the node points for the border intersections
+    // NOT a hyperedge or a hanging node (because border points)
     nodes.push_back(
         connManager.createNode(NodeData{false, coordinates0, false}));
     nodes.push_back(
@@ -56,58 +68,69 @@ public:
     nodes.push_back(
         connManager.createNode(NodeData{false, coordinates3, false}));
 
-    //        nodes.push_back(connManager.createNode(NodeData{false,
-    //        Coordinates{west_border, south_border}, false}));
-    //        nodes.push_back(connManager.createNode(NodeData{false,
-    //        Coordinates{west_border, north_border}, false}));
-    //        nodes.push_back(connManager.createNode(NodeData{false,
-    //        Coordinates{east_border, south_border}, false}));
-    //        nodes.push_back(connManager.createNode(NodeData{false,
-    //        Coordinates{east_border, north_border}, false}));
+    //nodes.push_back(connManager.createNode(NodeData{false,
+    //Coordinates{west_border, south_border}, false}));
+    //nodes.push_back(connManager.createNode(NodeData{false,
+    //Coordinates{west_border, north_border}, false}));
+    //nodes.push_back(connManager.createNode(NodeData{false,
+    //Coordinates{east_border, south_border}, false}));
+    //nodes.push_back(connManager.createNode(NodeData{false,
+    //Coordinates{east_border, north_border}, false}));
 
-    double length1 = nodes[0]->getData().getCoords().dist(
+    // 0 = SW, 1 = NW, 2 = SE, 3 = NE
+    double leftBorderLength = nodes[0]->getData().getCoords().dist(
         nodes[1]->getData().getCoords(), version2D);
-    double length2 = nodes[1]->getData().getCoords().dist(
+    double topBorderLength = nodes[1]->getData().getCoords().dist(
         nodes[3]->getData().getCoords(), version2D);
-    double length3 = nodes[2]->getData().getCoords().dist(
+    double rightBorderLength = nodes[2]->getData().getCoords().dist(
         nodes[3]->getData().getCoords(), version2D);
-    double length4 = nodes[0]->getData().getCoords().dist(
+    double bottomBorderLength = nodes[0]->getData().getCoords().dist(
         nodes[2]->getData().getCoords(), version2D);
-    double length5 = nodes[3]->getData().getCoords().dist(
+    double SWtoNELength = nodes[3]->getData().getCoords().dist(
         nodes[0]->getData().getCoords(), version2D);
 
+    // @todo can refactor some of the below to make less redundant
+
+    // create 5 edges
+    // left border creation
     connManager.createEdge(
         nodes[0], nodes[1], true,
         Coordinates{west_border, (north_border + south_border) / 2.,
                     map.get_height(west_border,
                                    (north_border + south_border) / 2., false)},
-        length1);
+        leftBorderLength);
+    // top border creation
     connManager.createEdge(
         nodes[1], nodes[3], true,
         Coordinates{(west_border + east_border) / 2., north_border,
                     map.get_height((west_border + east_border) / 2.,
                                    north_border, false)},
-        length2);
+        topBorderLength);
+    // right border creation
     connManager.createEdge(
         nodes[2], nodes[3], true,
         Coordinates{east_border, (north_border + south_border) / 2.,
                     map.get_height(east_border,
                                    (north_border + south_border) / 2., false)},
-        length3);
+        rightBorderLength);
+    // left border creation
     connManager.createEdge(
         nodes[0], nodes[2], true,
         Coordinates{(west_border + east_border) / 2., south_border,
                     map.get_height((west_border + east_border) / 2.,
                                    south_border, false)},
-        length4);
+        bottomBorderLength);
+    // this edge is diagonal from SW to NE with middle point being the center
+    // of the map; this is the common edge of the first 2 triangles
     connManager.createEdge(
         nodes[3], nodes[0], false,
         Coordinates{(west_border + east_border) / 2.,
                     (north_border + south_border) / 2.,
                     map.get_height((west_border + east_border) / 2.,
                                    (north_border + south_border) / 2., false)},
-        length5);
+        SWtoNELength);
 
+    // creates the hyperedge for the 2 initial triangles of the graph
     connManager.createInterior(nodes[0], nodes[1], nodes[3]);
     connManager.createInterior(nodes[0], nodes[3], nodes[2]);
   }
